b65efea0c4ea030bd671dfc9db3ec61ed90df287
Стив <kenethra.74@gmail.com>
2022-12-18 14:22:28 -0600

    feature(backend): add husky

--- src/web/apps/backend/src/routes/index.ts
+++ src/web/apps/backend/src/routes/index.ts
@@ -1,12 +1,12 @@
-import {Router} from 'express';
+import { Router } from 'express';
 import glob from 'glob';
 
 export function registerRoutes(router: Router) {
   const routes = glob.sync(__dirname + '/**/*.route.*');
-  routes.map(route => register(route, router))
+  routes.map(route => register(route, router));
 }
 
-function register(routePath: string, app: Router) {
+function register(routePath: string, router: Router) {
   const route = require(routePath);
-  route.register(app);
+  route.register(router);
 }
