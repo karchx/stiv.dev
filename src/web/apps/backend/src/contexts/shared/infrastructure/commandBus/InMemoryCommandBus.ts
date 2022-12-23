d670a37e9eef763505ef62039337bdf10813a491
Стив <kenethra.74@gmail.com>
2022-12-22 21:58:48 -0600

    feat(backend): command bus and handlers

---
+++ src/web/apps/backend/src/contexts/shared/infrastructure/commandBus/InMemoryCommandBus.ts
@@ -0,0 +1,14 @@
+import {Command} from "../../domain/Command";
+import {CommandBus} from "../../domain/CommandBus";
+import {CommandHandlers} from "./CommandHandlers";
+
+export class InMemoryCommandBus implements CommandBus {
+  constructor(private commandHandlers: CommandHandlers) {}
+
+  async dispatch(command: Command): Promise<void> {
+    console.log("dispatch");
+    const handler = this.commandHandlers.get(command);
+
+    await handler.handle(command);
+  }
+}
