7f9e4f5ba2b0929927940640c3cec537fb79bc41
Стив <kenethra.74@gmail.com>
2022-12-24 11:16:14 -0600

    feat(backend): command registered error

--- src/web/apps/backend/src/contexts/shared/infrastructure/commandBus/CommandHandlers.ts
+++ src/web/apps/backend/src/contexts/shared/infrastructure/commandBus/CommandHandlers.ts
@@ -1,5 +1,6 @@
 import { Command } from "../../domain/Command";
 import { CommandHandler } from "../../domain/CommandHandler";
+import { CommandNotRegisteredError } from "../../domain/CommandNotRegisteredError";
 
 export class CommandHandlers extends Map<Command, CommandHandler<Command>> {
   constructor(commandHandlers: CommandHandler<Command>[]) {
@@ -14,8 +15,7 @@ export class CommandHandlers extends Map<Command, CommandHandler<Command>> {
     const commandHandler = super.get(command.constructor);
 
     if (!commandHandler) {
-      console.error("Error");
-      throw "Error";
+      throw new CommandNotRegisteredError(command);
     }
 
     return commandHandler;
