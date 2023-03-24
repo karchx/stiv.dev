03f0893ade11dd301656b1893e46330f57f4e563
Стив <kenethra.74@gmail.com>
2023-03-23 19:22:03 -0600

    feat(portafolio): fix constructor

--- src/web/apps/portafolio/src/app/pages/projects/jtxs/jtxs.component.ts
+++ src/web/apps/portafolio/src/app/pages/projects/jtxs/jtxs.component.ts
@@ -1,9 +1,9 @@
-import {Component, OnDestroy, OnInit} from "@angular/core";
-import {Subscription} from "rxjs";
-import {delay} from "rxjs/operators";
-import {ModalService} from "../../../services/modal.service";
-import {IProject, Project} from "../../../models/projects.model";
-import {ProjectService} from "../../../service/project.service";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Subscription } from "rxjs";
+import { delay } from "rxjs/operators";
+import { ModalService } from "../../../services/modal.service";
+import { IProject, Project } from "../../../models/projects.model";
+import { ProjectService } from "../../../service/project.service";
 
 @Component({
   selector: "app-jtxs",
@@ -15,15 +15,15 @@ export class JtxsComponent implements OnInit, OnDestroy {
   imgSubs!: Subscription;
   defaultProjects: IProject[] = [
     {
-      description: "Animation (or game?) With canvas and trying a few things with javascript (nerd)",
+      description:
+        "Animation (or game?) With canvas and trying a few things with javascript (nerd)",
       github: "https://github.com/karchx/wwsx",
       image: "uploads/default.png",
       tags: ["javascript", "js", "ts"],
       title: "wwsx",
-      web: "https://karchx.github.io/wwsx/
+      web: "https://karchx.github.io/wwsx/"
     }
-  ]
-
+  ];
 
   constructor(
     private _projectService: ProjectService,
@@ -42,7 +42,7 @@ export class JtxsComponent implements OnInit, OnDestroy {
   }
 
   public getProjects() {
-    this._projectService.loadProjects().subscribe(({projects}) => {
+    this._projectService.loadProjects().subscribe(({ projects }) => {
       this.projects = projects.filter(c => c.tags.includes("js"));
     });
   }
